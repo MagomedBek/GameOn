bb94235c895cb63131cde785831dbbd94cbcf987
Mago <mbekbuza@gmail.com>
2022-07-14 19:21:18 +0200

    1.0

--- modal.js
+++ modal.js
@@ -7,13 +7,31 @@ function editNav() {
   }
 }
 
+
+function validation(){
+ 
+
+}
+validModal();
+
+
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const close = document.querySelector(".close");
+const submit = document.querySelector(".btn-submit");
+const body =document.querySelector(".modal-body");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+close.addEventListener("click", closeModal); 
+
+//valid event
+submit.addEventListener("click", validModal);
 
 // launch modal form
 function launchModal() {
@@ -21,3 +39,21 @@ function launchModal() {
 }
 
 
+//close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+// Validation
+function validModal() {
+  document.querySelector('#reserve input[type="submit"]' ).addEventListener("click", (e) => {
+    e.preventDefault();
+    
+    let valid = document.querySelector('#reserve').reportValidity();
+      if(valid)
+      {
+        body.innerHTML = "Merci ! Votre réservation a été reçue."
+      }
+  })
+  
+}
